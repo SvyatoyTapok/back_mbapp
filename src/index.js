35131b6fb01e7db6510179ed9d32d24818c2716f
SvyatoyTapok <ewfbewrb@mail.ru>
2025-05-27 21:47:20 +0300

    done backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import { db } from './db.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-
+app.post('/', async (req, res) => {
+  console.log(`Попытка входа с ключом: ${req.body.ApiKey}`, new Date().toLocaleString(), `IP: ${req.ip}`);
+  try {
+    const response = await db.execute('SELECT * FROM staff WHERE api_key = ?',
+      [req.body.ApiKey]
+    );
+    if (response[0].length === 1) {
+      res.status(200).json({ data: response[0][0] });
+    }
+    else {
+      res.status(401).json({
+        status: 'error',
+        message: 'Invalid API key',
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Backend running on http://localhost:${port}`);
